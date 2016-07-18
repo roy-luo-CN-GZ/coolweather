abf4e0d2a41001700bf4f996e444a804b2a7bc92
Roy <156912881@qq.com>
2016-07-18 23:36:13 +0800

    完成遍历省市县三级列表的功能.

---
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,223 @@
+package com.coolweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.coolweather.app.R;
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChooseAreaActivity extends Activity{
+	
+	public static final int LEVEL_PROVINCE=0;
+	
+	public static final int LEVEL_CITY=1;
+	
+	public static final int LEVEL_COUNTY=2;
+	
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private List<String> dataList=new ArrayList<String>();
+	
+	private CoolWeatherDB coolWeatherDB;
+	
+	private List<Province> provinceList;	//省列表
+	private List<City> cityList;			//市列表
+	private List<County> countyList; 		//县列表
+	
+	private Province selectedProvince;	//选中的省
+	private City selectedCity;       //选中的市
+	private int currentLevel;		//当前选中的级别
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(com.coolweather.app.R.layout.choose_area);
+		titleText=(TextView) findViewById(R.id.title_text);
+		listView=(ListView) findViewById(R.id.list_view);
+		adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO 自动生成的方法存根
+				if(currentLevel==LEVEL_PROVINCE){
+					selectedProvince=provinceList.get(position);
+					queryCities();
+				}else if(currentLevel==LEVEL_CITY){
+					selectedCity=cityList.get(position);
+					queryCounties();
+				}
+				
+			}
+			
+		});
+		coolWeatherDB=CoolWeatherDB.getInstance(this);
+		queryProvinces();
+		
+	}
+	
+	/**
+	 * 查询所有省信息,优先从数据库查询,如果没有结果再去服务器查询
+	 */
+	private void queryProvinces(){
+		provinceList=coolWeatherDB.loadProvinces();
+		if(provinceList.size()>0){
+			dataList.clear();
+			for(Province province:provinceList){
+				dataList.add(province.getProvinceName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText("中国");
+			currentLevel=LEVEL_PROVINCE;
+		}else{
+			queryFromServer(null, "province");
+		}
+	}
+	
+	/**
+	 * 查询所有市信息,优先从数据库查询,如果没有结果再去服务器查询
+	 */
+	private void queryCities(){
+		cityList=coolWeatherDB.loadCities(selectedProvince.getId());
+		if(cityList.size()>0){
+			dataList.clear();
+			for(City city:cityList){
+				dataList.add(city.getCityName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedProvince.getProvinceName());
+			currentLevel=LEVEL_CITY;
+		}else{
+			queryFromServer(selectedProvince.getProvinceCode(), "city");
+		}
+	}
+	
+	private void queryCounties(){
+		countyList=coolWeatherDB.loadCounties(selectedCity.getId());
+		if(countyList.size()>0){
+			dataList.clear();
+			for(County county:countyList){
+				dataList.add(county.getCountyName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedCity.getCityName());
+			currentLevel=LEVEL_COUNTY;
+		}else{
+			queryFromServer(selectedCity.getCityCode(), "county");
+		}
+	}
+	
+	/**
+	 * 从服务器获得数据
+	 * @param code
+	 * @param type
+	 */
+	private void queryFromServer(String code,final String type){
+		String address;
+		if(!TextUtils.isEmpty(code)){
+			address="http://www.weather.com.cn/data/list3/city"+code+".xml";
+		}else{
+			address="http://www.weather.com.cn/data/list3/city.xml";
+		}
+		showProgressDialog();
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+			
+			@Override
+			public void onFinish(String response) {
+				// TODO 自动生成的方法存根
+				boolean result=false;
+				if("province".equals(type)){
+					result=Utility.handleProvincesResponse(coolWeatherDB, response);
+				}else if("city".equals(type)){
+					result=Utility.handleCitiesResponse(coolWeatherDB, response, selectedProvince.getId());
+				}else if("county".equals(type)){
+					result=Utility.handleCountiesResponse(coolWeatherDB, response, selectedCity.getId());
+				}
+				if(result){
+					//通过runOnUiThread()方法回到主线程处理逻辑
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							// TODO 自动生成的方法存根
+							closeProgressDialog();
+							if("province".equals(type)){
+								queryProvinces();
+							}else if("city".equals(type)){
+								queryCities();
+							}else if("county".equals(type)){
+								queryCounties();
+							}
+						}
+					});
+				}
+			}
+			
+			@Override
+			public void onError(Exception e) {
+				// TODO 自动生成的方法存根
+				
+			}
+		});
+	}
+	
+	/**
+	 * 显示进度对话框
+	 */
+	private void showProgressDialog(){
+		if(progressDialog==null){
+			progressDialog=new ProgressDialog(this);
+			progressDialog.setMessage("正在加载中...");
+			progressDialog.setCanceledOnTouchOutside(false);
+		}
+		progressDialog.show();
+	}
+	
+	/**
+	 * 关闭进度对话框
+	 */
+	private void closeProgressDialog(){
+		if(progressDialog!=null){
+			progressDialog.dismiss();
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		// TODO 自动生成的方法存根
+		if(currentLevel==LEVEL_COUNTY){
+			queryCities();
+		}else if(currentLevel==LEVEL_CITY){
+			queryProvinces();
+		}else{
+			finish();
+		}
+	}
+	
+}
--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -11,7 +11,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 	 * Province±ÌΩ®±Ì”Ôæ‰
 	 */
 	public static final String CREATE_PROVINCE="create table Province(" +
-			"id integer autoincrement primary key," +
+			"id integer primary key autoincrement," +
 			"province_name text," +
 			"province_code text)";
 	
